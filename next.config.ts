b124715662c05e1e3fd36ae963edbaccafddc08e
Kacper Budny <kacperbudny98@gmail.com>
2026-08-29 20:00:27 +0200

    Improve local setup

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import "./src/env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["127.0.0.1"],
   reactCompiler: true,
 };
 
