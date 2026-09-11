17e1ee1065beb8a5b904d1114535bc5bdcf522f8
Kacper Budny <kacperbudny98@gmail.com>
2026-09-11 16:40:53 +0200

    Fixes

--- src/lib/theme.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test";
-import { THEME_COLOR } from "@/lib/theme";
-
-test("browser chrome color is the near-black canvas, not white", () => {
-  // sRGB of the visual session canvas oklch(0.09 0 0)
-  expect(THEME_COLOR).toBe("#020202");
-});
